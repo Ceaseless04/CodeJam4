fa3ce39221deb2cdd50aa9afb0897eb073f4073a
Kristian Vazquez <jmfdusq@jmfamily.com>
2025-10-30 11:04:57 -0400

    Finished: displaying random paragraph based on json file

--- src/terminal.js
+++ src/terminal.js
@@ -1,4 +1,20 @@
 import prompts from "prompts";
 import chalk from "chalk";
+import fs from 'fs';
 
-// Loading dummy paragraph
+// Loading paragraphs
+const paragraphs = JSON.parse(fs.readFileSync('../paragraphs.json', 'utf8'));
+const original = paragraphs[Math.floor(Math.random() * paragraphs.length)];
+
+const shouldEnd = false;
+
+console.log(chalk.blue("Welcome to the Speed Typing Test!"));
+await prompts({ type: 'text', name: 'ready', message: chalk.blue('Press Enter when ready...') });
+
+console.log(chalk.blue("\n Type the following paragraph:\n"));
+console.log(chalk.yellow(`"${original}"\n`));
+
+// Gameloop
+while (shouldEnd) {
+  
+}
